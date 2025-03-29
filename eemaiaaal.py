4293b061ea2f1255524d8a24611dff0b73814dae
Uzair <uzairuzair050@gmail.com>
2025-03-29 13:22:07 +0500

    Added functionality of attaching images

--- eemaiaaal.py
+++ eemaiaaal.py
@@ -1,14 +1,26 @@
 import smtplib
 import os
+import imghdr
 from email.message import EmailMessage
 email_id = os.environ.get("EMAIL_USER") 
 email_pass = os.environ.get("EMAIL_PASS")
 
 msg = EmailMessage()
-msg['Subject'] = 'Fight again Corona Virus'
+msg['Subject'] = 'Check out these images'
 msg['From'] = email_id
-msg['To'] = 'Receiver Email Address'
-msg.set_content("Hey, Hi let's fight against corona virus by staying at home")
+msg['To'] = 'Receover Email Address'
+msg.set_content("Image has been Attached !!")
+
+files = ['Hello.jpg','Hello1.jpg']
+for file in files:
+
+    with open(file,'rb') as m:
+       file_data = m.read()
+       file_type = imghdr.what(m.name)
+       file_name = m.name
+       print(file_type)
+
+    msg.add_attachment(file_data, maintype = 'image', subtype = file_type,filename = file_name)
 
 
 with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
