4151dd4574f9b12f4cb57a56c9cbbcfacacd3c30
Uzair <uzairuzair050@gmail.com>
2025-03-29 12:57:27 +0500

    Set Format of Gmail
    Setting Format of gmail to make code look good and understandable

--- eemaiaaal.py
+++ eemaiaaal.py
@@ -1,19 +1,22 @@
 import smtplib
 import os
+from email.message import EmailMessage
 email_id = os.environ.get("EMAIL_USER") 
 email_pass = os.environ.get("EMAIL_PASS")
+
+msg = EmailMessage()
+msg['Subject'] = 'Fight again Corona Virus'
+msg['From'] = email_id
+msg['To'] = 'Receiver Email Address'
+msg.set_content("Hey, Hi let's fight against corona virus by staying at home")
+
+
 with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
 
-    # ehlo is an alternative to helo for services that support the smtp services extenstions
-
+  
     smtp.login(email_id,email_pass)
 
-    subject = 'Fight again Corona Virus'
-    body = "Hey, Hi let's fight against corona virus by staying at home"
+    smtp.send_message(msg)
 
-    msg = f'Subject:{subject}\n\n\n{body}'
-    smtp.sendmail(email_id,'#Receiver Email Address',msg)
-
-    # format here is senders email id  --- receivers email id, =--- msg
 
 
